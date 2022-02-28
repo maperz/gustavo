6600c9119cdbd765e1211f44d8f85d0c1e7a7c71
Markus Perz <markus.perz@gmail.com>
2022-02-28 12:00:09 +0100

    Change install script

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-mkdir /opt/maperz/gustavo/
+mkdir -p /opt/maperz/gustavo/
 cp main.js /opt/maperz/gustavo/
 cp .env /opt/maperz/gustavo/
+cp package-lock.json /opt/maperz/gustavo/
+cp package.json /opt/maperz/gustavo/
+npm i
 
 # Copy service file to system services
 cp installation/gustavo.service /lib/systemd/system/
